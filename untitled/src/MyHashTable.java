2bd9bf847456040d30f403e6610b5dc528e78c39
freezge <freezge@gmail.com>
2023-05-13 14:12:34 +0600

    Starting point

---
+++ untitled/src/MyHashTable.java
@@ -0,0 +1,24 @@
+public class MyHashTable<K, V>{
+    private class HashNode<K, V>{}
+    private HashNode <K, V>[] chainArray;
+    private int M = 11;
+    private int size;
+    public MyHashTable(){}
+    public MyHashTable(int M){}
+    private int hash(K key){
+        return 0;
+    }
+    public void put(K key, V value){}
+    public V get(K key){
+        return null;
+    }
+    public V remove(K key){
+        return null;
+    }
+    public boolean contains(V Value){
+        return false;
+    }
+    public K getKey(V value){
+        return null;
+    }
+}
