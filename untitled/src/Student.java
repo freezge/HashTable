e8603f46e712cd9676d01e782e2b3b300bfea062
freezge <freezge@gmail.com>
2023-05-15 01:10:47 +0600

    class Student

---
+++ untitled/src/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    private String fullName;
+    private String group;
+
+    public Student(String fullName, String group) {
+        this.fullName = fullName;
+        this.group = group;
+    }
+}
