cfcb7b6801ac480c1a6998252ea8e2fe7e8865bb
freezge <freezge@gmail.com>
2023-05-15 01:57:01 +0600

    comments

--- untitled/src/MyHashTable.java
+++ untitled/src/MyHashTable.java
@@ -15,54 +15,54 @@ public class MyHashTable<K, V>{
     private HashNode <K, V>[] chainArray;
     private int len = 11;
     private int size = 0;
-    public MyHashTable(){
+    public MyHashTable(){//constructor
         this.chainArray = new HashNode[len];
     }
-    public MyHashTable(int len){
+    public MyHashTable(int len){//constructor
         this.len = len;
         this.chainArray = new HashNode[len];
     }
     private int hash(K key){
-        if (key == null) {
+        if (key == null) {//checking if key is not empty
             return 0;
         }
-        int hashCode = 1;
+        int hashCode = 1;//can put any prime number here
         char[] arr = key.toString().toCharArray();
         for (int i = 0; i < arr.length; i++) {
             char c = arr[i];
             hashCode = 13 * hashCode + c;
         }
         hashCode = hashCode % len;
-        return hashCode;
+        return hashCode;//returning index
     }
     public void put(K key, V value){
         int hash = hash(key);
         HashNode<K, V> hashNode = new HashNode<K, V>(key, value);
         HashNode<K, V> rootNode = chainArray[hash];
-        if(rootNode == null){
-            chainArray[hash] = hashNode;
-            size++;
-            return;
+        if(rootNode == null){//checking if its first entered element
+            chainArray[hash] = hashNode;//input in array
+            size++;//increasing size
+            return;//end of func here
         }
-        while(rootNode.next != null){
-            if (rootNode.key.equals(key)){
-                rootNode.value = value;
-                return;
+        while(rootNode.next != null){//if it's not first moving to the end
+            if (rootNode.key.equals(key)){//if we already have the same key
+                rootNode.value = value;//we change its value
+                return;//end of func here
             }
             rootNode = rootNode.next;
         }
-        if (rootNode.key.equals(key)){
+        if (rootNode.key.equals(key)){//doing the same again for the last element
             rootNode.value = value;
             return;
         }
-        rootNode.next = hashNode;
+        rootNode.next = hashNode;//adding to the end
         size++;
     }
     public V get(K key){
         int hash = hash(key);
         HashNode<K, V> rootNode = chainArray[hash];
-        while(rootNode != null){
-            if(rootNode.key.equals(key)){
+        while(rootNode != null){//loop to move through array
+            if(rootNode.key.equals(key)){//if we found it we return its value
                 return rootNode.value;
             }
             rootNode = rootNode.next;
@@ -72,18 +72,18 @@ public class MyHashTable<K, V>{
     public V remove(K key){
         int hash = hash(key);
         HashNode<K, V> rootNode = chainArray[hash];
-        if (rootNode == null){
+        if (rootNode == null){//checking for emptiness
             return null;
         }
-        if(rootNode.key.equals(key)){
+        if(rootNode.key.equals(key)){//checking needed element is the first
             chainArray[hash] = rootNode.next;
             size--;
             return rootNode.value;
         }
-        while(rootNode.next != null){
-            if(rootNode.next.key.equals(key)){
-                rootNode.next = rootNode.next.next;
-                size--;
+        while(rootNode.next != null){//loop to move through array
+            if(rootNode.next.key.equals(key)){//condition
+                rootNode.next = rootNode.next.next;//removing element here
+                size--;//decreasing size
                 return rootNode.next.value;
             }
             rootNode = rootNode.next;
@@ -91,13 +91,13 @@ public class MyHashTable<K, V>{
         return null;
     }
     public boolean contains(V value){
-        return getKey(value) != null;
+        return getKey(value) != null;//if getKey found smthng it exists
     }
     public K getKey(V value){
-        for (int i = 0; i < len; i++){
+        for (int i = 0; i < len; i++){//for every bucket
             HashNode<K, V> rootNode = chainArray[i];
-            while(rootNode != null){
-                if (rootNode.value.equals(value)){
+            while(rootNode != null){//loop to move through array
+                if (rootNode.value.equals(value)){//if we found value we return its key
                     return rootNode.key;
                 }
                 rootNode = rootNode.next;
@@ -105,7 +105,7 @@ public class MyHashTable<K, V>{
         }
         return null;
     }
-    public void testing(){
+    public void testing(){//printing number of elements in each bucket
         for (int i = 0; i < len; i++) {
             HashNode<K, V> rootNode = chainArray[i];
             int counter = 0;
